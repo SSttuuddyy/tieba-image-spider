1b038df1f2c32699db434980be6773855f596d99
SSttuuddyy <2606847877@qq.com>
2023-04-12 21:45:10 +0800

    添加了gui窗口和打开下载的文件夹的功能，便于操作

--- 贴吧爬虫.py
+++ 贴吧爬虫.py
@@ -1,22 +1,8 @@
 from utils import Spieder
+from gui import Gui
 
 if __name__=='__main__':
-    print('以Edge浏览器为准')
-    y = input('贴子号：')
-    sp = Spieder(y)
-    num1 = int(input('起始页码：'))
-    num2 = int(input('终止页码：'))
-    film = input('目标文件夹：')
-    page_list=sp.get_page(num2, num1)
-    count = num1
-    url4 = sp.page_list[0]
-    html=sp.get_html(url4)
-    name_list = sp.parse_txt(html)
-    path = film+'//'+name_list[0]+'//'
-    sp.mkdir(path, name_list[0])
-    for url in page_list:
-        html=sp.get_html(url)
-        img_list, title_list=sp.parse_html(html)
-        sp.download(path, img_list, title_list)
-        print(' 第 %s 页图片下载完成' % count)
-        count = count + 1
+    gui = Gui()
+    sp = Spieder()
+    gui.sp = sp
+    gui.gene()
